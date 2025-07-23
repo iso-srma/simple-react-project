ec0df2febfcdfeb1a2539b4da1a06430c6ae982c
Ishwor Sharma <isosrma@gmail.com>
2025-07-23 17:56:49 +0545

    todos

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 const App = () => {
   const [todos, setTodos] = useState(["Eat", "code", "sleep", "repeat"]);
 
+  const addData = () => {
+    setTodos([...todos, userInput])
+  }
+  
   return (
     <div>
       <h1>Todo App</h1>
@@ -10,8 +14,8 @@ const App = () => {
       <input type='text'></input>
       <button>Add</button>
       <ul>
-        {todos.map((todo) => (
-          <li>{todo}</li>
+        {todos.map((todo , index) => (
+          <li key={index}>{todo}</li>
         ))}
       </ul>
     </div>
