dd0c84309ff0e07ca4ed234ae250db1bd4d7ac12
Ishwor Sharma <isosrma@gmail.com>
2025-07-23 18:02:02 +0545

    todos add

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [todos, setTodos] = useState(["Eat", "code", "sleep", "repeat"]);
+  const [userInput, setUserInput] = useState("");
 
+  const [todos, setTodos] = useState([]);
+
+  
   const addData = () => {
     setTodos([...todos, userInput])
   }
-  
+
   return (
     <div>
       <h1>Todo App</h1>
       <p>Welcome to the Todo App!</p>
-      <input type='text'></input>
-      <button>Add</button>
+      <input onChange={(e)=> setUserInput(e.target.value)} type='text'></input>
+      <button onClick={()=> addData()}>Add</button>
       <ul>
         {todos.map((todo , index) => (
           <li key={index}>{todo}</li>
