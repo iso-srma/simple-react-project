7d443767def5939a2e88d608c8a315eb8d59a1da
Ishwor Sharma <isosrma@gmail.com>
2025-07-28 14:38:46 +0545

    "adding Logo and video "

--- converting-existing-htmlpage/convert_to/src/components/About.js
+++ converting-existing-htmlpage/convert_to/src/components/About.js
@@ -8,7 +8,19 @@ function About() {
       <section id="about">
         <h2>About Me</h2>
       <img src={profileImage} alt="Ishwor Sharma" className="profile-img" />
-        <p>
+          <video 
+          width="320" 
+          height="240" 
+          controls
+          autoPlay
+        muted
+        loop 
+          className="intro-video"
+        >
+          <source src="/song.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+       <p>
             Hello! I'm a passionate web developer with experience in building simple and beautiful websites. I love learning new technologies and creating projects that solve real-world problems.
         </p>
     </section>
