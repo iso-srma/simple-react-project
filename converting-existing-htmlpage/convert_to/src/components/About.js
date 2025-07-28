ed3b18ed65f6c3603b5e16817361005253077af8
Ishwor Sharma <isosrma@gmail.com>
2025-07-28 14:14:45 +0545

    "adding photo"

--- converting-existing-htmlpage/convert_to/src/components/About.js
+++ converting-existing-htmlpage/convert_to/src/components/About.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import profileImage from '../assets/1.jpg';
+import './About.css'
 
 function About() {
   return (
     <div>
       <section id="about">
         <h2>About Me</h2>
+      <img src={profileImage} alt="Ishwor Sharma" className="profile-img" />
         <p>
             Hello! I'm a passionate web developer with experience in building simple and beautiful websites. I love learning new technologies and creating projects that solve real-world problems.
         </p>
